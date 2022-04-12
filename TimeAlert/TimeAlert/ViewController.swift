d927bfdcecfb99c4afc1774c557627f247ba31ca
MinjiJo <story3697@gmail.com>
2022-04-12 21:14:58 +0900

    Alert Equal Time

---
+++ TimeAlert/TimeAlert/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  TimeAlert
+//
+//  Created by Minji Jo on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    var alarmTime: String?
+    var alertFlag = false
+
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간 : " + formatter.string(from: datePickerView.date)
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+ 
+    @objc func updateTime() {
+        let date = NSDate()
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간 : " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        if (alarmTime == currentTime) {
+            if !alertFlag {
+                let lampOnAlert = UIAlertController(title: "알림", message: "설정된 시간입니다😍", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "네, 알겠습니다.", style: UIAlertAction.Style.default, handler: nil)
+                lampOnAlert.addAction(onAction)
+                present(lampOnAlert, animated: true, completion: nil)
+                alertFlag = true
+            }
+        } else {
+            alertFlag = false
+        }
+    }
+
+}
+
